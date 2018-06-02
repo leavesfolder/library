10a48f4d05d5efa3a51d2edc52eef4e41deffa8e
leavesfolder <1056868147@qq.com>
2018-06-02 13:11:41 +0800

    首次修改提交测试

--- src/main/java/com/library/user/controller/UserController.java
+++ src/main/java/com/library/user/controller/UserController.java
@@ -83,6 +83,7 @@ public class UserController {
             String name = (String) enu.nextElement();
             System.out.println("退出："+name);
             session.removeAttribute(name);
+            System.out.println("一次测试");
 
         }
         return new ResultModel(true);
